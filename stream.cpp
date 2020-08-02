e1d19573047e22d2b35882fcef6508fc1c77c9a0
netricks <netricks@protonmail.com>
2020-08-03 02:06:54 +0300

    'a'

--- stream.cpp
+++ stream.cpp
@@ -4,6 +4,33 @@
 
 #include <crow/gates/udpgate.h>
 
+#include <chrono>
+#include <thread>
+
+using namespace std::chrono_literals;
+
+bool is_open = false;
+cv::VideoCapture cap;
+
+void open_camera()
+{
+	if (!cap.open(0, cv::CAP_V4L2))
+	{
+		std::cout << "error on camera open" << std::endl;
+		return;
+	}
+
+	cap.set(cv::CAP_PROP_FRAME_WIDTH, 640);
+	cap.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
+	is_open = true;
+}
+
+void close_camera()
+{
+	cap.release();
+	is_open = false;
+}
+
 int main(int argc, char** argv)
 {
 	crow::spammer spammer;
@@ -12,18 +39,6 @@ int main(int argc, char** argv)
 	spammer.bind(1);
 
 	int sts;
-
-	cv::VideoCapture cap;
-
-	if (!cap.open(0, cv::CAP_V4L2))
-	{
-		std::cout << "error on camera open" << std::endl;
-		return 0;
-	}
-
-	cap.set(cv::CAP_PROP_FRAME_WIDTH, 640);
-	cap.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
-
 	udpgate.bind(12);
 
 	//crow::diagnostic_enable();
@@ -36,18 +51,30 @@ int main(int argc, char** argv)
 		std::vector<uint8_t> buffer;
 
 		cv::Mat frame, frame3;
-		cap >> frame;
 
-		cv::resize(frame, frame3, cv::Size(640*3/4, 480*3/4 ));
+		if (spammer.count_of_subscribers())
+		{
+			if (is_open == false)
+			{
+				open_camera();
+			}
 
-		sts = cv::imencode(".jpg", frame3, buffer);
+			cap >> frame;
+			cv::resize(frame, frame3, cv::Size(640 / 4, 480 / 4 ));
+			sts = cv::imencode(".jpg", frame3, buffer);
 
-		//nos::println(buffer.size());
-		spammer.send({buffer.data(), buffer.size()});
+			nos::println(buffer.size());
+			spammer.send({buffer.data(), buffer.size()});
+		}
+		else
+		{
+			if (is_open == true)
+			{
+				close_camera();
+			}
+		}
 
-		//auto frame2 = cv::imdecode(buffer, cv::IMREAD_COLOR);
-		//cv::imshow("viewer", frame2);
-		//cv::waitKey(1);
+		std::this_thread::sleep_for(20ms);
 	}
 
 	return 0;
