32f2d6a41fef487ff3a44e495353cffc5d7445b7
netricks <netricks@protonmail.com>
2020-08-09 02:18:59 +0300

    'a'

--- viewer.cpp
+++ viewer.cpp
@@ -6,20 +6,42 @@
 
 #include <thread>
 
+#include <wels/codec_api.h>
+
 void routine(igris::buffer message)
 {
-	static int inited = 0;
+	uint8_t Y[1024*1024];
+	uint8_t U[1024*1024];
+	uint8_t V[1024*1024];
 
-	auto frame = cv::imdecode({message.data(), (int)message.size()}, cv::IMREAD_COLOR);
-	
-	if (!inited) 
-	{
-		inited = 1;
-		cv::resizeWindow("viewer", frame.size().width, frame.size().height);
-	}
+	/*	static int inited = 0;
 
-	cv::imshow("viewer", frame);	
-	cv::waitKey(1);
+		auto frame = cv::imdecode({message.data(), (int)message.size()}, cv::IMREAD_COLOR);
+
+		if (!inited)
+		{
+			inited = 1;
+			cv::resizeWindow("viewer", frame.size().width, frame.size().height);
+		}
+
+		cv::imshow("viewer", frame);
+		cv::waitKey(1);*/
+
+	ISVCDecoder *pSvcDecoder;
+//input: encoded bitstream start position; should include start code prefix
+	unsigned char *pBuf = (unsigned char *)message.data();
+//input: encoded bit stream length; should include the size of start code prefix
+	int iSize = message.size();
+//output: [0~2] for Y,U,V buffer for Decoding only
+	unsigned char *pData[3] = {Y,U,V};
+//in-out: for Decoding only: declare and initialize the output buffer info, this should never co-exist with Parsing only
+	SBufferInfo sDstBufInfo;
+	memset(&sDstBufInfo, 0, sizeof(SBufferInfo));
+//in-out: for Parsing only: declare and initialize the output bitstream buffer info for parse only, this should never co-exist with Decoding only
+	SParserBsInfo sDstParseInfo;
+	memset(&sDstParseInfo, 0, sizeof(SParserBsInfo));
+	//sDstParseInfo.pDstBuff = new unsigned char[PARSE_SIZE]; //In Parsing only, allocate enough buffer to save transcoded bitstream for a frame
+
 }
 
 int main(int argc, char** argv)
@@ -30,7 +52,7 @@ int main(int argc, char** argv)
 	udpgate.bind(12);
 	subs.bind(1);
 
-	if (argc < 2) 
+	if (argc < 2)
 	{
 		nos::println("Usage: viewer CROWKER_ADDRESS");
 		exit(0);
