5a9abfd707275ba8441661bb129bf35eabf450cb
mirmik <mirmikns@yandex.ru>
2022-07-31 23:58:13 +0300

    'a'

--- stream.cpp
+++ stream.cpp
@@ -92,71 +92,80 @@ int main(int argc, char **argv)
     crow::start_spin();
     nos::println("start_spin ... ok");
 
-    for (;;)
+    std::thread thr;
+
+    for (int i = 0; i < 2; ++i)
     {
-        std::vector<uint8_t> buffer;
-
-        cv::Mat frame, frame3, gray;
-
-        for (int i = 0; i < 2; ++i)
-        {
-            if (spammer[i].count_of_subscribers())
+        thr = std::thread(
+            [&]()
             {
-                if (camera[i].is_open == false)
+                std::vector<uint8_t> buffer;
+
+                cv::Mat frame, frame3, gray;
+                for (;;)
                 {
-                    camera[i].open_camera();
+
+                    if (spammer[i].count_of_subscribers())
+                    {
+                        if (camera[i].is_open == false)
+                        {
+                            camera[i].open_camera();
+                        }
+
+                        auto start_ms = std::chrono::duration_cast<
+                                            std::chrono::milliseconds>(
+                                            std::chrono::system_clock::now()
+                                                .time_since_epoch())
+                                            .count();
+
+                        // cv::Mat planes[3];
+
+                        camera[i].cap >> frame;
+                        cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
+                        nos::println(gray.size(), gray.step);
+
+                        // split(gray, planes);  // planes[2] is the red channel
+
+                        if (mode == "jpg")
+                        {
+                            std::vector<int> params;
+                            params.push_back(cv::IMWRITE_JPEG_QUALITY);
+                            params.push_back(quality);
+
+                            sts = cv::imencode(".jpg", gray, buffer, params);
+
+                            auto end_ms = std::chrono::duration_cast<
+                                              std::chrono::milliseconds>(
+                                              std::chrono::system_clock::now()
+                                                  .time_since_epoch())
+                                              .count();
+
+                            spammer[i].send(
+                                {(const char *)buffer.data(), buffer.size()});
+
+                            auto end2_ms = std::chrono::duration_cast<
+                                               std::chrono::milliseconds>(
+                                               std::chrono::system_clock::now()
+                                                   .time_since_epoch())
+                                               .count();
+
+                            nos::println(buffer.size(),
+                                         start_ms,
+                                         end_ms - start_ms,
+                                         end2_ms - start_ms);
+                        }
+                    }
+                    else
+                    {
+                        if (camera[i].is_open)
+                        {
+                            camera[i].close_camera();
+                        }
+                    }
+
+                    std::this_thread::sleep_for(30ms);
                 }
-
-                auto start_ms =
-                    std::chrono::duration_cast<std::chrono::milliseconds>(
-                        std::chrono::system_clock::now().time_since_epoch())
-                        .count();
-
-                // cv::Mat planes[3];
-
-                camera[i].cap >> frame;
-                cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
-                nos::println(gray.size(), gray.step);
-
-                // split(gray, planes);  // planes[2] is the red channel
-
-                if (mode == "jpg")
-                {
-                    std::vector<int> params;
-                    params.push_back(cv::IMWRITE_JPEG_QUALITY);
-                    params.push_back(quality);
-
-                    sts = cv::imencode(".jpg", gray, buffer, params);
-
-                    auto end_ms =
-                        std::chrono::duration_cast<std::chrono::milliseconds>(
-                            std::chrono::system_clock::now().time_since_epoch())
-                            .count();
-
-                    spammer[i].send(
-                        {(const char *)buffer.data(), buffer.size()});
-
-                    auto end2_ms =
-                        std::chrono::duration_cast<std::chrono::milliseconds>(
-                            std::chrono::system_clock::now().time_since_epoch())
-                            .count();
-
-                    nos::println(buffer.size(),
-                                 start_ms,
-                                 end_ms - start_ms,
-                                 end2_ms - start_ms);
-                }
-            }
-            else
-            {
-                if (camera[i].is_open)
-                {
-                    camera[i].close_camera();
-                }
-            }
-
-            std::this_thread::sleep_for(30ms);
-        }
+            });
     }
     return 0;
 }
